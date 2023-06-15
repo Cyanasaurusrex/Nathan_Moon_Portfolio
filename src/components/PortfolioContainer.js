a9a4258607c13879cd2945b1a9b3a7fd81f3b402
Nathan Moon <moonman700@gmail.com>
2023-06-14 21:41:02 -0400

    Added navbar navigation and highlighting

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,36 @@
-import React from "react";
-import Header from './Header'
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import AboutMe from './pages/AboutMe';
+import ContactMe from './pages/ContactMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 import Footer from './Footer'
 
-export default function PortfolioContainer() {
-    return(
-    <div>
-        <Header />
-        <Footer />
-    </div>
-    )
 
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'ContactMe') {
+      return <ContactMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>      
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
 }
