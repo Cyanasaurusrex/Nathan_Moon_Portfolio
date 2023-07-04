702b0a6456d8df6d7a61e4232a83da9a16bef0ff
Nathan Moon <moonman700@gmail.com>
2023-07-04 17:19:14 -0400

    added framework for portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 "use Client";
 
 export default function AboutMe() {
-  return( <p>About Me</p>)
+  return( <p>Developer with a strong background in manufacturing and engineering, offering a unique perspective that bridges the gap between technology and industry-specific challenges. Skilled in leveraging cutting-edge technologies like React and Node.js to develop intuitive and scalable web applications, constantly seeking opportunities to learn and adapt to emerging trends, fostering innovation and pushing the boundaries of software development. Demonstrates a strong ability to collaborate effectively in cross-functional teams, consistently delivering results on time and exceeding project expectations.
+    </p>)
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,32 @@
 "use Client";
+import Link from "next/link";
 
 export default function Portfolio() {
-  return <p>Portfolio</p>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <Link href="https://github.com/CodeByDex/OIODev">OIO Dev</Link>
+        <p className=" italic">In Progress</p>
+        <p>
+          Connect with skilled Technology Professionals who specialize in
+          creating exceptional websites and server applications. We bring your
+          digital vision to life!
+        </p>
+      </div>
+      <div className="flex flex-row justify-between">
+        <Link href="https://www.putasporkinit.co/">PutaSporkinit</Link>
+        <p>
+          Bringing the magic of your favorite movies, TV shows, and video games
+          to your plate with mouth-watering recipes!
+        </p>
+      </div>
+      <div className="flex flex-row justify-between">
+        <Link href="https://moxufo.github.io/Rave-Reviews/">Rave Reviews</Link>
+        <p>
+          Craft a night out by finding local events, lodging, and dining. Let us
+          build you a night to remember!
+        </p>
+      </div>
+    </div>
+  );
 }
